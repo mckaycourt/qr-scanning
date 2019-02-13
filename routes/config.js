e9b8311c7703c655e3d2c7a56e255b564d1fc127
mckaycourt <mckay.court@gmail.com>
2019-02-13 13:58:59 -0700

    Fixed the permissions on the html page

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    getConfig: {
-
+    getConfig: function () {
+        return {
+            host: '127.0.0.1',
+            user: 'mmcourt',
+            password: 'Mlhlt2200!',
+            database: 'voting'
+        }
+    },
+    getURL: function () {
+        return "http://localhost:3000";
+    },
+    getLocation: function () {
+        return "";
     }
 };
